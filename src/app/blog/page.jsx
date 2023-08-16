99abef29c2424738b1ffdfb99d7f5684b328d11c
vvvolkova-92 <saiviolet@gmail.com>
2023-08-16 18:39:17 +0300

    BLOG / POSTITEM / Страница + стили

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import styles from './style.module.css';
+import PostItem from "@/components/PostItem/PostItem";
 const Blog = () => {
   return (
     <section className={styles.wrapper}>
+      <PostItem
+        url="/blog/testId"
+        title="Lorem ipsum dolor sit amet, consectetur."
+        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consectetur cupiditate, doloremque earum eum ex exercitationem in iste maxime molestiae nihil perspiciatis quasi quia quod, rem saepe soluta unde vel?"
+        image="https://imgv3.fotor.com/images/blog-richtext-image/part-blurry-image.jpg"
+      />
+      <PostItem
+        url="#"
+        title="Lorem ipsum dolor sit amet, consectetur."
+        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consectetur cupiditate, doloremque earum eum ex exercitationem in iste maxime molestiae nihil perspiciatis quasi quia quod, rem saepe soluta unde vel?"
+        image="https://imgv3.fotor.com/images/blog-richtext-image/part-blurry-image.jpg"
+      />
+      <PostItem
+        url="#"
+        title="Lorem ipsum dolor sit amet, consectetur."
+        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consectetur cupiditate, doloremque earum eum ex exercitationem in iste maxime molestiae nihil perspiciatis quasi quia quod, rem saepe soluta unde vel?"
+        image="https://imgv3.fotor.com/images/blog-richtext-image/part-blurry-image.jpg"
+      />
+
 
     </section>
   );
---
+++ src/components/PostItem/PostItem.jsx
@@ -0,0 +1,20 @@
+"use client"
+import React from 'react';
+import styles from './postItem.module.css';
+import Image from "next/image";
+import Link from "next/link";
+const PostItem = ({title, description, image, url = "#"}) => {
+  return (
+    <Link className={styles.wrapper} href={url}>
+      <div className={styles.imageWrapper}>
+        <Image className={styles.image} src={image} alt={`Изображение к посту ${title}`} fill/>
+      </div>
+      <div className={styles.content}>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.description}>{description}</p>
+      </div>
+    </Link>
+  );
+}
+
+export default PostItem;
